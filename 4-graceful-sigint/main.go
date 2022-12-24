a7ffd2bd8ad7d6986200007aa63b0e93688a7942
Jin Wei <seetohjinwei@gmail.com>
2022-12-24 21:01:59 +0800

    Solve 4

--- 4-graceful-sigint/main.go
+++ 4-graceful-sigint/main.go
@@ -13,9 +13,27 @@
 
 package main
 
+import (
+	"os"
+	"os/signal"
+)
+
 func main() {
 	// Create a process
 	proc := MockProcess{}
+
+	// Help from: https://stackoverflow.com/questions/11268943/is-it-possible-to-capture-a-ctrlc-signal-sigint-and-run-a-cleanup-function-i
+	sigint := make(chan os.Signal)
+	signal.Notify(sigint, os.Interrupt)
+
+	go func() {
+		<-sigint       // first SIGINT
+		go proc.Stop() // run this in a separate goroutine so that it won't block this goroutine
+
+		<-sigint // second SIGINT
+		close(sigint)
+		os.Exit(1)
+	}()
 
 	// Run the process (blocking)
 	proc.Run()
