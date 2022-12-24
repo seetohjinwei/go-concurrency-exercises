d32fb1f20bce276185dacc42e6f713a3e95c0096
Jin Wei <seetohjinwei@gmail.com>
2022-12-24 20:40:41 +0800

    Solve 3-advanced

--- 3-limit-service-time/README.md
+++ 3-limit-service-time/README.md
@@ -7,3 +7,18 @@ service will kill your process, unless you are a paid premium user.
 Beginner Level: 10s max per request
 Advanced Level: 10s max per user (accumulated)
 
+**Advanced Output**
+
+```
+❯ go run -race .
+UserID: 0       Process 1 started.
+UserID: 1       Process 2 started.
+UserID: 0       Process 3 started.
+UserID: 0       Process 4 started.
+UserID: 1       Process 5 started.
+UserID: 0       Process 1 done.
+UserID: 0       Process 3 killed. (No quota left)
+UserID: 0       Process 4 killed. (No quota left)
+UserID: 1       Process 5 done.
+UserID: 1       Process 2 done.
+```
--- 3-limit-service-time/main.go
+++ 3-limit-service-time/main.go
@@ -10,7 +10,10 @@
 
 package main
 
-import "time"
+import (
+	"sync"
+	"time"
+)
 
 // User defines the UserModel. Use this to check whether a User is a
 // Premium user or not
@@ -19,6 +22,8 @@ type User struct {
 	IsPremium bool
 	TimeUsed  int64 // in seconds
 }
+
+var mu sync.Mutex
 
 // HandleRequest runs the processes requested by users. Returns false
 // if process had to be killed
@@ -30,15 +35,24 @@ func HandleRequest(process func(), u *User) bool {
 		return true
 	}
 
+	start := time.Now()
+
 	go func() {
 		process()
 		close(check)
 	}()
 
+	// mutex for modifying u.TimeUsed
+	mu.Lock()
+	defer mu.Unlock()
+
 	select {
 	case <-check:
-		return true
-	case <-time.After(10 * time.Second):
+		end := time.Now()
+		elapsed := end.Sub(start)
+		u.TimeUsed += int64(elapsed)
+		return u.TimeUsed <= int64(10*time.Second)
+	case <-time.After(10*time.Second - time.Duration(u.TimeUsed)):
 		return false
 	}
 }
