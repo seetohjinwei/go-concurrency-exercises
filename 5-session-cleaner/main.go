fee73fddf5a0ddbc5ff2d8fc8030e3ac18d7217c
Jin Wei <seetohjinwei@gmail.com>
2022-12-25 12:34:35 +0800

    Solve 5

--- 5-session-cleaner/main.go
+++ 5-session-cleaner/main.go
@@ -20,37 +20,55 @@ package main
 import (
 	"errors"
 	"log"
+	"sync"
+	"time"
 )
 
 // SessionManager keeps track of all sessions from creation, updating
 // to destroying.
 type SessionManager struct {
+	mu       sync.Mutex
 	sessions map[string]Session
+	quit     chan struct{}
 }
 
 // Session stores the session's data
 type Session struct {
-	Data map[string]interface{}
+	Data        map[string]interface{}
+	LastUpdated time.Time
 }
+
+var (
+	cleanPeriod = time.Second
+	cleanExpiry = 5 * time.Second
+)
 
 // NewSessionManager creates a new sessionManager
 func NewSessionManager() *SessionManager {
 	m := &SessionManager{
 		sessions: make(map[string]Session),
+		quit:     make(chan struct{}),
 	}
+
+	// initialise the session cleaner
+	go m.cleanSessions(cleanPeriod, cleanExpiry)
 
 	return m
 }
 
 // CreateSession creates a new session and returns the sessionID
 func (m *SessionManager) CreateSession() (string, error) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
 	sessionID, err := MakeSessionID()
 	if err != nil {
 		return "", err
 	}
 
 	m.sessions[sessionID] = Session{
-		Data: make(map[string]interface{}),
+		Data:        make(map[string]interface{}),
+		LastUpdated: time.Now(),
 	}
 
 	return sessionID, nil
@@ -63,6 +81,9 @@ var ErrSessionNotFound = errors.New("SessionID does not exists")
 // GetSessionData returns data related to session if sessionID is
 // found, errors otherwise
 func (m *SessionManager) GetSessionData(sessionID string) (map[string]interface{}, error) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
 	session, ok := m.sessions[sessionID]
 	if !ok {
 		return nil, ErrSessionNotFound
@@ -72,6 +93,9 @@ func (m *SessionManager) GetSessionData(sessionID string) (map[string]interface{
 
 // UpdateSessionData overwrites the old session data with the new one
 func (m *SessionManager) UpdateSessionData(sessionID string, data map[string]interface{}) error {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
 	_, ok := m.sessions[sessionID]
 	if !ok {
 		return ErrSessionNotFound
@@ -79,10 +103,45 @@ func (m *SessionManager) UpdateSessionData(sessionID string, data map[string]int
 
 	// Hint: you should renew expiry of the session here
 	m.sessions[sessionID] = Session{
-		Data: data,
+		Data:        data,
+		LastUpdated: time.Now(),
 	}
 
 	return nil
+}
+
+// Ticker method adapted from: https://stackoverflow.com/a/16466581
+func (m *SessionManager) cleanSessions(period time.Duration, expiry time.Duration) {
+	ticker := time.NewTicker(period)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			m.mu.Lock()
+
+			for key, session := range m.sessions {
+				now := time.Now()
+				if now.Sub(session.LastUpdated) >= expiry {
+					// delete expired session
+					delete(m.sessions, key)
+				}
+			}
+
+			m.mu.Unlock()
+		case <-m.quit:
+			return
+		}
+	}
+}
+
+func (m *SessionManager) KillSessionCleaner() {
+	if m.quit == nil {
+		return
+	}
+
+	close(m.quit)
+	m.quit = nil
 }
 
 func main() {
